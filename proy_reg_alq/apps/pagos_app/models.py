3ae91650987afacf923b8a2b5697e4492e18f5c9
Mariano <mariano.ramaidana@gmail.com>
2023-04-04 12:34:07 -0300

    se creó la primer aplicación y se hicieron migraciones

--- proy_reg_alq/apps/pagos_app/models.py
+++ proy_reg_alq/apps/pagos_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class pago(models.Model):
+    numero = models.IntegerField(blank=True, null=True)
+    fecha_de_pago = models.DateField(blank=True, null=True)
+    periodo_correspondiente = models.CharField(max_length=25)
+    importe_alquiler = models.FloatField(max_length=10)
+    importe_electricidad = models.FloatField(max_length=10)
+    importe_agua = models.FloatField(max_length=10)
+    pagado = models.BooleanField
